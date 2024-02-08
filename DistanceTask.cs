603f061ce428435eaaef042bf7bcbcc49e319014
sonoma <wllamsmth1@gmail.com>
2024-02-08 21:58:41 +0300

    update

--- DistanceTask.cs
+++ DistanceTask.cs
@@ -8,28 +8,28 @@ namespace DistanceTask
         public static double GetDistanceToSegment(double ax, double ay, double bx, double by, double x, double y)
         {
             // Вычисляем вектора AB и AP
-            double abx = bx - ax;
-            double aby = by - ay;
-            double apx = x - ax;
-            double apy = y - ay;
+            double abx = bx - ax; // Координаты вектора AB по оси x
+            double aby = by - ay; // Координаты вектора AB по оси y
+            double apx = x - ax; // Координаты вектора AP по оси x
+            double apy = y - ay; // Координаты вектора AP по оси y
 
             // Скалярное произведение векторов AB и AP
             double abDotAp = abx * apx + aby * apy;
 
             // Если скалярное произведение меньше нуля, то проекция точки на отрезок лежит за точкой A
             if (abDotAp <= 0)
-                return Math.Sqrt(apx * apx + apy * apy);
+                return Math.Sqrt(apx * apx + apy * apy); // Расстояние от точки до точки A
 
             // Скалярное произведение векторов BA и BP
-            double bax = ax - bx;
-            double bay = ay - by;
-            double bpx = x - bx;
-            double bpy = y - by;
+            double bax = ax - bx; // Координаты вектора BA по оси x
+            double bay = ay - by; // Координаты вектора BA по оси y
+            double bpx = x - bx; // Координаты вектора BP по оси x
+            double bpy = y - by; // Координаты вектора BP по оси y
             double baDotBp = bax * bpx + bay * bpy;
 
             // Если скалярное произведение меньше нуля, то проекция точки на отрезок лежит за точкой B
             if (baDotBp <= 0)
-                return Math.Sqrt(bpx * bpx + bpy * bpy);
+                return Math.Sqrt(bpx * bpx + bpy * bpy); // Расстояние от точки до точки B
 
             // Вычисляем площадь треугольника ABP
             double area = Math.Abs(abx * bpy - aby * bpx);
@@ -38,7 +38,7 @@ namespace DistanceTask
             double lengthAB = Math.Sqrt(abx * abx + aby * aby);
 
             // Расстояние до отрезка равно дважды площади треугольника ABP, делённой на длину AB
-            return area / lengthAB;
+            return area / lengthAB; // Возвращаем расстояние до отрезка
         }
     }
 }
